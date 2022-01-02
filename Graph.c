f0f446b23a7f0ac2b040bce89dd1b5d305f8c4e8
zivm20 <ziv.morgan@gmail.com>
2022-01-02 23:07:42 +0200

    remade the graph implementation

--- Graph.c
+++ Graph.c
@@ -3,125 +3,155 @@
 #include "Graph.h"
 
 
+
+
 void del_out_edges(pNode node){
-  node->edges_size = 0;
-  free(node->edges);
-  node->edges = NULL;
+  pEdege temp;
+  while(node->edges != NULL){
+    temp = node->edges;
+    node->edges = node->edges->next;
+    free(temp);
+  }
 }
 
 void del_in_edges(pGraph g, pNode node){
-  for(int i=0; i<g->size; i++){
-    pNode src_node = get_node(g,i);
-    if(src_node){
-      if(src_node->edges_size > node->id){
-        if(src_node->edges[node->id] != NULL){
-          free(src_node->edges[node->id]);
-          src_node->edges[node->id]=NULL;
+  pNode current_node = g->head;
+  while(current_node != NULL){
+    pEdge current_edge = current_node->edges;
+    pEdge temp;
+    if(current_edge != NULL && current_edge->dest == node->id){
+      current_node->edges = current_edge->next;
+      free(current_edge);
+    }
+    else{
+      while(current_edge->next != NULL && current_edge->dest < node->id){
+        if(current_edge->next->dest == node->id){
+          current_edge->next = temp;
+          current_edge->next = current_edge->next->next;
+          free(temp);
         }
+        current_edge = current_edge->next;
       }
     }
+    current_node = current_node->next;
   }
-
 }
 
 void init_graph(pGraph g, int size){
-  g->head = malloc(size * sizeof(pNode) );
+  g->head = malloc(sizeof(Node));
   g->size = size;
 }
 
-void add_node(pGraph g,int id){
-  if(g->size < id){
-    g->head = realloc(g->head, (id+1)*sizeof(pNode));
-    g->size = id+1;
-    if(g->head == NULL){
-      delete_graph(g);
-      printf("memory error, unable to alocate memory to add node!");
-      exit(0);
-    }
-  }
+pNode createNode(int id){
+    pNode n = malloc(sizeof(Node));
+    n->id = id;
+    n->edges = NULL;
+    n->next = NULL;
+    n->edges_size = 0;
+}
 
-  if(g->head[id]){
-      del_out_edges(g->head[id]);
+void add_node(pGraph g,int id){
+  pNode current;
+
+  if(g->head != NULL || g->head->id >= id){
+    if(g->head->id==id){
+      del_out_edges(current->next);
+    }
+    else{
+      pNode temp = createNode(id);
+      temp->next = g->head;
+      g->head = temp;
+    }
   }
   else{
-    g->head[id] = malloc(sizeof(Node));
-    g->head[id]->id = id;
-    g->head[id]->edges_size = 0;
-    g->head[id]->edges = NULL;
-
-  }
-}
-void add_edge(pGraph g, int node1,int dest,int weight){
-
-
-  pNode src = get_node(g,node1);
-
-
-  if(src->edges==NULL){
-    src->edges = malloc((dest+1)*sizeof(pEdge));
-  }
-  else if(src->edges_size < dest){
-    src->edges = realloc(src->edges, sizeof(pEdge)*(dest+1));
-    src->edges_size = dest;
-    if(src->edges==NULL){
-      delete_graph(g);
-      printf("memory error, unable to alocate memory to add node!");
-      exit(0);
+    current = g->head;
+    while(current->next != NULL && current->next->id < id){
+      current = current->next;
+    }
+    if(current->next->id==id){
+      del_out_edges(current->next);
+    }
+    else{
+      pNode temp = createNode(id);
+      temp->next = current->next;
+      current->next = temp;
     }
   }
-  src->edges[dest] = malloc(sizeof(Edge));
-  src->edges[dest]->weight = weight;
-  src->edges[dest]->dest = dest;
-  src->edges_size+=1;
+}
+
+void add_edge(pGraph g, int node1,int dest,int weight){
+  pNode node = get_node(g,node1);
+  pEdge current;
+  if(node->edges != NULL || node->edges->dest >= dest){
+    if(node->edges->dest==dest){
+      del_out_edges(current->next);
+    }
+    else{
+      pEdge temp = malloc(sizeof(Edge));
+      temp->next = NULL;
+      temp->weight t =weight;
+      temp->dest = dest;
+      temp->next = node->edges;
+      node->edges = temp;
+    }
+  }
+  else{
+    current = node->edges;
+    while(current->next != NULL && current->next->dest < dest){
+      current = current->next;
+    }
+    if(current->next->dest==id){
+      del_out_edges(current->next);
+    }
+    else{
+      pEdge temp = malloc(sizeof(Edge));
+      temp->next = NULL;
+      temp->weight t =weight;
+      temp->dest = dest;
+      temp->next = current->next;
+      current->next = temp;
+    }
+  }
 }
 
 void print_graph(pGraph g){
-  for(int src=0; src<g->size; src++){
-    pNode node = get_node(g,src);
-    if(node){
-      printf("%d: ",node->id);
-      for(int dest=0; dest<node->edges_size; dest++){
-        if(node->edges[dest] != NULL){
-          printf("(%d -> %d) (%d) ",node->id,node->edges[dest]->dest,node->edges[dest]->weight);
-        }
-      }
-      printf("\n");
+  pNode current_node = g->head;
+  while(current_node != NULL){
+    pEdge current_edge = current_node->edges;
+    while(current_edge->next != NULL){
+      printf("(%d -> %d)(%d) ",current_node->id,current_edge->dest,current_edge->weight);
+      current_edge = current_edge->next;
     }
+    current_node = current_node->next;
   }
 }
 
-
 void delete_graph(pGraph g){
-  for(int i=0; i<g->size; i++){
-    if(get_node(g,i)!=NULL){
-      delete_node(g,get_node(g,i));
-    }
-  }
-  printf("deleated all\n");
-  free(g->head);
-  printf("deleated head\n");
-  free(g);
-  printf("freed graph\n");
+  pNode current;
+  while(g->head != NULL){
 
-  g = NULL;
+    delete_node(g,g->head)
+    current = g->head;
+    g->head = g->head->next;
+    free(current);
+  }
+  free(g);
 }
 
 void delete_node(pGraph g,pNode node){
   del_in_edges(g,node);
-  printf("del in edges complete for %d\n",node->id);
+  //printf("del in edges complete for %d\n",node->id);
   del_out_edges(node);
-  printf("del out edges complete for %d\n",node->id);
-  int n = node->id;
-  free(node);
-  node = NULL;
-
+  //printf("del out edges complete for %d\n",node->id);
 }
 
 pNode get_node(pGraph g, int id){
-
-  if(g->size > id){
-
-    return g->head[id];
+  pNode current = g->head;
+  while(current->id <= id){
+    if(current->id == id ){
+      return current;
+    }
+    current=current->next;
   }
   return NULL;
 }
--- Graph.h
+++ Graph.h
@@ -1,26 +1,26 @@
 #ifndef GRAPH_
 #define GRAPH_
 
-typedef struct Node_ *pNode;
-#define CHILD_TO_PARENT = 0, PARENT_TO_CHILD = 1
 
 
 typedef struct Edge_{
   int weight;
   int dest;
-
+  struct Edge_ *next;
 }Edge;
 typedef Edge* pEdge;
 
 typedef struct Node_{
-  int id, edges_size;
-  pEdge *edges;
+  int id;
+  pEdge edges;
+  struct Node_ *next;
 }Node;
 typedef Node* pNode;
 
+
 typedef struct Graph_{
   int size;
-  pNode *head;
+  pNode head;
 }Graph;
 typedef Graph* pGraph;
 
